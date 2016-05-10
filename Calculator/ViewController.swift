286c3c046122d5ff44e3d59a60e4b30da0a6eb04
Noah Patterson <noah@noahpatterson.me>
2016-05-10 07:12:20 -0400

    first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Noah Patterson on 5/10/16.
+//  Copyright © 2016 Noah Patterson. All rights reserved.
+//
+
+//importing a module - all public classes are imported. Model might import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+//    instance variables are called properties in Swift
+    
+    
+}
+
