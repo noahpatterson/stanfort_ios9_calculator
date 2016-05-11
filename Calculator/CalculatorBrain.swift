5af9b9949052611f4aa81e91965bf10b88b442e3
Noah Patterson <noah@noahpatterson.me>
2016-05-11 16:40:04 -0400

    move logic to an enum of associated values. Add cos and e to layout

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -16,14 +16,34 @@ class CalculatorBrain {
         accumulator = operand
     }
     
-    var operations: Dictionary<String, Double>  = [
-        "π" : M_PI,
-        "e" : M_E
+    var operations: Dictionary<String, Operation>  = [
+        "π" : Operation.Constant(M_PI), //M_PI,
+        "e" : Operation.Constant(M_E), //M_E,
+        "√" : Operation.UnaryOperation(sqrt), //sqrt,
+        "cos" : Operation.UnaryOperation(cos) //cos
     ]
     
+    // enums can have methods, but no vars, no inheritance
+    enum Operation {
+        case Constant(Double)
+        /* unaryOperation defines an associated value that is a function, that takes a double
+        , that returns a Double = func(Double) -> Double */
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation
+        case Equals
+    }
+    
     func performOperation(symbol: String) {
-        if let constant = operations[symbol] {
-            accumulator = constant
+//        if let constant = operations[symbol] {
+//            accumulator = constant
+//        }
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let associatedConstantValue): accumulator = associatedConstantValue
+            case .UnaryOperation(let function): accumulator = function(accumulator)
+            case .BinaryOperation: break
+            case .Equals: break
+            }
         }
     }
     
