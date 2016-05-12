103e44d5e3fcfeea297ee9357ca68d72a0451741
Noah Patterson <noah@noahpatterson.me>
2016-05-12 07:20:10 -0400

    Handle binary operations.
    + Allow binary operations to string calculations.
    + Add a clear button.
    + On 'Equals' clear binary's 'pending' variable

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+func multiply(firstNumber: Double, secondNumber: Double) -> Double {
+    return firstNumber * secondNumber
+}
+
 class CalculatorBrain {
     
     private var accumulator = 0.0
@@ -17,10 +21,13 @@ class CalculatorBrain {
     }
     
     var operations: Dictionary<String, Operation>  = [
+        "clear" : Operation.Constant(0.0),
         "π" : Operation.Constant(M_PI), //M_PI,
         "e" : Operation.Constant(M_E), //M_E,
         "√" : Operation.UnaryOperation(sqrt), //sqrt,
-        "cos" : Operation.UnaryOperation(cos) //cos
+        "cos" : Operation.UnaryOperation(cos),//cos
+        "✕" : Operation.BinaryOperation(multiply),
+        "=" : Operation.Equals
     ]
     
     // enums can have methods, but no vars, no inheritance
@@ -29,9 +36,15 @@ class CalculatorBrain {
         /* unaryOperation defines an associated value that is a function, that takes a double
         , that returns a Double = func(Double) -> Double */
         case UnaryOperation((Double) -> Double)
-        case BinaryOperation
+        case BinaryOperation((Double, Double) -> Double)
         case Equals
     }
+    
+    private func intermediateEquals(pending: PendingBinaryOperationInfo) -> Double {
+        return pending.binaryFunction(pending.firstOperand, accumulator)
+    }
+    
+    
     
     func performOperation(symbol: String) {
 //        if let constant = operations[symbol] {
@@ -41,10 +54,26 @@ class CalculatorBrain {
             switch operation {
             case .Constant(let associatedConstantValue): accumulator = associatedConstantValue
             case .UnaryOperation(let function): accumulator = function(accumulator)
-            case .BinaryOperation: break
-            case .Equals: break
+            case .BinaryOperation(let function):
+                if pending != nil {
+                    accumulator = intermediateEquals(pending!)
+                }
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals:
+                if pending != nil {
+                    accumulator = intermediateEquals(pending!)
+                    pending = nil
+                }
             }
         }
+    }
+    
+    var pending: PendingBinaryOperationInfo?
+    
+    // all of the structs vars are part of the 'free' initializer
+    struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
     }
     
     
@@ -53,4 +82,6 @@ class CalculatorBrain {
             return accumulator
         }
     }
+    
+    
 }
